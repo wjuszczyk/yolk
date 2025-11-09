c8cb8c82b066458bf94e1d3fdf8c24ca6e9228ff
Wojciech Juszczyk <wojciech.juszczyk@gmail.com>
2025-11-09 01:50:46 +0100

    mpris, vpn changes

--- eggs/polybar/scripts/vpn-wireguard-wg.sh
+++ eggs/polybar/scripts/vpn-wireguard-wg.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-if [[ $(
-  sudo wg show all | grep wg0 2>&1 >/dev/null
-  echo $?
-) -eq 0 ]]; then
-  echo "VPN: up"
+VPN=$(sudo wg show | awk '/wg0/ {print $2}')
+
+if [ -n ${VPN} ]; then
+  echo "VPN: ${VPN}"
 else
   echo "VPN: down"
 fi
